25fbad694f1739632e1a2f0f1eaa052be39737c1
Sonny Michael <isonnymichael@gmail.com>
2025-07-06 13:30:25 +0700

    feat: add husky

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ["commitlint.config.js", "**/*.config.js"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      // Aturan custom Anda
+    },
+  },
+];
